2265ab7e4328e3149a92d97326890f28f49ea15e
erick <erickkirui653@gmail.com>
2018-07-19 09:59:14 +0300

    java scrip linked

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,10 @@ $(document).ready(function() {
      var inputtedLastName = $("input#new-last-name").val();
 
      var newContact = new Contact(inputtedFirstName, inputtedLastName);
+     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+    });
+  });
+  
